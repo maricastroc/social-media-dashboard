01f0d117426e652ae4ee9882114fed1ccda47c84
Mariana Castro <marianacastrorc@gmail.com>
2023-05-02 22:05:32 -0300

    Fixing semantic issues

--- src/components/Majorcard.tsx
+++ src/components/Majorcard.tsx
@@ -15,18 +15,18 @@ export function Majorcard(props: MajorcardProps) {
   return (
     <div className={styles.majorcard}>
       <div className={styles[props.variant]}></div>
-      <header>
+      <div className={styles.username_section}>
         <img src={props.src} alt="Logo Social Media" />
         <p>{props.username}</p>
-      </header>
-      <main>
+      </div>
+      <div className={styles.followers_section}>
         <h1>{props.followers}</h1>
         <p>{props.follow_type}</p>
-      </main>
-      <footer className={props.up ? styles.up : styles.down}>
+      </div>
+      <div className={props.up ? styles.up : styles.down}>
         <img src="./images/icon-up.svg" alt="Followers Data" />
         <span>{props.data} Today</span>
-      </footer>
+      </div>
     </div>
   )
 }
